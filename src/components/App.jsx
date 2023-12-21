398fc4133b35419939e6ca4f2f279f7e684dc785
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2023-12-22 04:14:53 +0530

    Add the basic code files

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function App() {
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString("en-IT", { hour12: false })
+  );
+
+  function currentTime() {
+    setTime(new Date().toLocaleTimeString("en-IT", { hour12: false }));
+  }
+  setInterval(currentTime, 1000);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={currentTime}> Get Time</button>
+    </div>
+  );
+}
+
+export default App;
